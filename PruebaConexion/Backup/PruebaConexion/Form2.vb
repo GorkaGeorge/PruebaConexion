81af08ee37f0a2cf397f2eeb6e9165c67cb8bb7d
GorkaGeorge <73058178+GorkaGeorge@users.noreply.github.com>
2020-10-18 13:45:11 +0200

    Add files via upload

---
+++ PruebaConexion/Backup/PruebaConexion/Form2.vb
@@ -0,0 +1,30 @@
+Imports system.data.OleDb.OleDbConnection
+Imports System.Data.OleDb
+
+
+Public Class Form2
+    Dim cn As New OleDbConnection
+    Dim da As New OleDbDataAdapter
+
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        MsgBox("vas a conectar a la Base de datos")
+
+        cn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" & _
+      "Data Source=C:\Users\Gorka\Documents\xtreme.mdb;"
+
+            cn.Open()
+        Console.WriteLine("Conexion esta a" & cn.State)
+            MsgBox(cn.State)
+            MsgBox("se ha abierto la conexion")
+       
+
+    End Sub
+
+
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
+
